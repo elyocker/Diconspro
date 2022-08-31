0941acbfafbdcdd5c71bd837ea3797a86f7f96c5
elyocker <jppaz16@misena.edu.co>
2022-08-30 21:52:43 -0500

    se modifico el texto

--- vistas/modulos/login.php
+++ vistas/modulos/login.php
@@ -6,7 +6,7 @@
         <!-- /.login-logo -->
         <div class="card card-outline card-primary">
             <div class="card-header text-center">
-                <a href="#" class="h1"><b>Diconspro</b></a>
+                <a href="#" class="h1"><b>PRUEBA EXITOSA</b></a>
             </div>
             <div class="card-body">
                  <p class="login-box-msg">Inicia sesión</p>
